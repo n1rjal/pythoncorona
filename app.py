fa1992db033b5b766db614f7a50b794a83133043
n1rjal <nirjalpaudel54312@gmail.com>
2020-04-05 01:29:08 +0545

    tried android interface

--- app.py
+++ app.py
@@ -4,16 +4,23 @@ import json
 import datetime
 import pandas as pd
 
-todaydate=str(datetime.date.today())
-todayfile="report {}.json".format(todaydate)
-with open('./reports/{}'.format(todayfile),"r") as filee:
-    datas=json.load(filee)
+def userdata(country):
+    todaydate=str(datetime.date.today())
+    todayfile="report {}.json".format(todaydate)
+    with open('./reports/{}'.format(todayfile),"r") as filee:
+        datas=json.load(filee)
 
-datafor=datas['nepal']
-heads=(datas['head'])
-print(heads)
-msg=""
-for i in range(1,6):
-    msg=msg+str(heads[i]+ " : " +datafor[i]+"\n")
+    datafor=datas[country]
+    heads=(datas['head'])
+    print(heads)
+    msg=""
+    for i in range(1,6):
+        msg=msg+str(heads[i]+ " : " +datafor[i]+"\n")
 
-notification.notify(datafor[0].upper(),msg)
+    reply=(datafor[0].upper(),msg)
+    return reply
+
+def notify(title,body): 
+    
+    notification.notify(title,body)   
+    
---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from app import userdata
+from app import notify
+
+
+class CustomWidget(Widget):
+
+    last_name_text_input = ObjectProperty()
+    ego = NumericProperty()
+    surname = StringProperty()
+    
+    def submit_surname(self):
+        self.name = self.last_name_text_input.text
+        print("Assign surname: {}".format(self.name))
+        self.title,self.body=userdata(self.name)
+        self.name = ''
+        notify(title=self.title,body=self.body)
+        self.last_name_text_input=""  
+
+class CustomWidgetApp(App):
+    def build(self):
+        return CustomWidget()
+
+if __name__ == "__main__":
+    CustomWidgetApp().run()
